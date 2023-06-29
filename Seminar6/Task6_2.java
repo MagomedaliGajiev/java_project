d8963a4ba8ecbe8e6ba387e88de96b64a404963d
Magomedali Gajiev <mag198421@gmail.com>
2023-06-30 00:51:59 +0300

    Some changes Seminar6/Task6_2

--- Seminar6/Task6_2.java
+++ Seminar6/Task6_2.java
@@ -7,6 +7,12 @@ public class Task6_2 {
 
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        cat1.name = "Barsil";
+        cat1.name = "Barsik";
+        cat1.getVaccine();
+        cat1.getVaccine();
+        cat1.petCat(5);
+        cat1.sex = true;
+        cat1.petCat(5);
+
     }
 }
